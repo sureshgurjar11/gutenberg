46751084b8c02e27334fefa770e0cc3cbc6ef260
Marin Atanasov <tyxla@abv.bg>
2021-07-14 13:24:23 +0300

    Use ColorIndicator and fix a bunch of styles

--- packages/edit-site/src/components/sidebar/global-styles-v2/components/nav-link.js
+++ packages/edit-site/src/components/sidebar/global-styles-v2/components/nav-link.js
@@ -5,10 +5,10 @@ import { Item, NavigatorLink } from '@wordpress/components';
 
 export const NavLink = ( props ) => {
 	return (
-		<Item action css={ { padding: 0 } }>
+		<Item action style={ { padding: 0 } }>
 			<NavigatorLink
 				{ ...props }
-				css={ { display: 'block', padding: '8px 12px' } }
+				style={ { display: 'block', padding: '8px 12px' } }
 			/>
 		</Item>
 	);
